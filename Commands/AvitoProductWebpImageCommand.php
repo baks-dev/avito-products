4611c829efae4c71ac437291a36f820f55b82136
kingstar <admin@baks.dev>
2024-09-22 14:40:01 +0300

    Baks Development

---
+++ Commands/AvitoProductWebpImageCommand.php
@@ -0,0 +1,97 @@
+<?php
+/*
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Avito\Products\Commands;
+
+use BaksDev\Avito\Products\Entity\Images\AvitoProductImage;
+use BaksDev\Avito\Products\Type\Image\AvitoProductImageUid;
+use BaksDev\Core\Doctrine\DBALQueryBuilder;
+use BaksDev\Core\Messenger\MessageDispatchInterface;
+use BaksDev\Files\Resources\Messenger\Request\Images\CDNUploadImage;
+use BaksDev\Files\Resources\Messenger\Request\Images\CDNUploadImageMessage;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'baks:avito-products:cdn',
+    description: 'Комманда отправляет на CDN файлы изображений '
+)]
+class AvitoProductWebpImageCommand extends Command
+{
+    public function __construct(
+        private readonly DBALQueryBuilder $DBALQueryBuilder,
+        private readonly MessageDispatchInterface $messageDispatch,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument('argument', InputArgument::OPTIONAL, 'Описание аргумента');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $progressBar = new ProgressBar($output);
+        $progressBar->start();
+
+
+        $dbal = $this->DBALQueryBuilder->createQueryBuilder(self::class);
+
+        $dbal
+            ->select('image.id')
+            ->addSelect('image.name')
+            ->from(AvitoProductImage::class, 'image')
+            ->where('image.cdn IS NOT TRUE');
+
+        $images = $dbal->fetchAllAssociative();
+
+        foreach($images as $image)
+        {
+            $message = new CDNUploadImageMessage(
+                new AvitoProductImageUid($image['id']),
+                AvitoProductImage::class,
+                $image['name']
+            );
+
+            $this->messageDispatch->dispatch($message);
+            $progressBar->advance();
+        }
+
+
+        $progressBar->finish();
+
+        $io->success('Команда успешно завершена');
+
+        return Command::SUCCESS;
+    }
+}
