5c6bcdfbb239327d5530e933f9a62a1a0145a296
admin <admin@baks.dev>
2025-05-10 23:22:50 +0300

    BaksDevelopment
    Signed-off-by: MichelAngelo <baksdevelopmen@gmail.com>

--- Controller/Admin/Tests/AvitoProductNewEditAdminControllerTest.php
+++ Controller/Admin/Tests/AvitoProductNewEditAdminControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -39,7 +39,7 @@ use Symfony\Component\DependencyInjection\Attribute\When;
  * @depends BaksDev\Avito\Products\UseCase\NewEdit\Tests\AvitoProductEditTest::class
  */
 #[When(env: 'test')]
-final class AvitoProductNewEditControllerTest extends WebTestCase
+final class AvitoProductNewEditAdminControllerTest extends WebTestCase
 {
     private const string ROLE = 'ROLE_AVITO_PRODUCTS_EDIT';
 
